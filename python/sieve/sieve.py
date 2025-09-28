7e666fd523b7015b8b649f275c55532790b101c1
ghostboy67 <55520860+ghostboy67@users.noreply.github.com>
2025-09-28 15:38:35 -0700

    modified algorithm

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -4,20 +4,19 @@ class Sieve:
     def __init__(self) -> None:
         i = 2
         x = 0
-        prime = False
-        while x < 10000:
-            f = 0
+        prime = True
+        while x <= 1000000:
+            f = 2
             while f < i:
-                if f != 0:
-                    if i % f != 0:
-                        prime = True
-                    elif i % f == 0:
-                        prime = False
-                        break
+                if i % f != 0:
+                    prime = True
+                elif i % f == 0:
+                    prime = False
+                    break
                 f += 1
             if prime:
                 self.prime_list.append(i)
-            x += 1
+                x += 1
             i += 1
 
     def nth_prime(self, n: int) -> int:
