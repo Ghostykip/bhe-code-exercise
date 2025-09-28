2fe7a351dcc1336458cee6715bcbfb70baa8e977
Riley Strader <55520860+Ghostykip@users.noreply.github.com>
2025-09-27 21:56:31 -0700

    added algorithm. to create array full of prime numbers

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,5 +1,24 @@
 class Sieve:
+    prime_list = list()
+
     def __init__(self) -> None:
-        pass
+        i = 2
+        x = 0
+        prime = False
+        while x < 10000:
+            f = 0
+            while f < i:
+                if f != 0:
+                    if i % f != 0:
+                        prime = True
+                    elif i % f == 0:
+                        prime = False
+                        break
+                f += 1
+            if prime:
+                self.prime_list.append(i)
+            x += 1
+            i += 1
+
     def nth_prime(self, n: int) -> int:
-        pass
+        return self.prime_list[n]
